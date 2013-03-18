37d2640b062cab0067dd4064ced6e2b835c36cdc
Dimitry Kloper <kloper@users.sf.net>
2013-03-19 00:03:23 +0200

    Moved do_load_bg_modelview() and do_insert_background() to boxfish_vrml_bg.cpp

--- boxfish/src/boxfish_vrml_bg.cpp
+++ boxfish/src/boxfish_vrml_bg.cpp
@@ -152,6 +152,120 @@ Control::generate_sky_arrays(
    return nvertexes;
 }
 
+void 
+Control::do_load_bg_modelview()
+{
+   using namespace openvrml;
+   dgd_scopef(trace_vrml);
+
+
+   mat4f opengl_modelview;
+   glGetFloatv( GL_MODELVIEW_MATRIX, (GLfloat*)opengl_modelview.mat );
+   dgd_echo(opengl_modelview);
+      
+   vec3f zero = make_vec3f(0,0,0);
+   std::vector<vec3f> axis;
+   axis.push_back( make_vec3f(1,0,0) );
+   axis.push_back( make_vec3f(0,1,0) );
+   axis.push_back( make_vec3f(0,0,1) );
+
+   vec3f zero_proj = zero * opengl_modelview;
+
+   dgd_echo(zero_proj);
+
+   for( std::vector<vec3f>::iterator iter = axis.begin();
+        iter != axis.end();
+        ++iter ) {
+      dgd_echo(*iter);
+      *iter = (*iter * opengl_modelview - zero_proj).normalize();
+      dgd_echo(*iter);
+   }
+
+   const mat4f& scene_rotation = make_mat4f(
+      axis[0].x(), axis[0].y(), axis[0].z(), 0,
+      axis[1].x(), axis[1].y(), axis[1].z(), 0,
+      axis[2].x(), axis[2].y(), axis[2].z(), 0,
+      0, 0, 0, 1
+   );
+   
+   dgd_echo(scene_rotation);
+   glLoadMatrixf( (const GLfloat*)scene_rotation.mat );
+}
+
+void
+Control::do_insert_background( const openvrml::background_node& n )
+{
+   using namespace openvrml;
+
+   dgd_scopef(trace_vrml);
+
+   glMatrixMode(GL_MODELVIEW);
+   glPushMatrix();
+
+   do_load_bg_modelview();
+      
+   if (execute_list(&n)) {
+      glPopMatrix();
+      return;
+   }
+
+   GLuint glid = 0;
+
+   if( this->m_select_mode == draw_mode ) {
+      glid = glGenLists(1);
+      glNewList(glid, GL_COMPILE_AND_EXECUTE);
+   }
+
+   boost::shared_array<Vector> vertexes;
+   boost::shared_array<Vector> normals;
+   boost::shared_array<Vector> texture;
+   boost::shared_array<Vector> colors;
+   index_layout_type           indexes;
+
+   float scene_max_size = 1.0f;
+   vec3f scene_center;
+
+   bool rc = get_scene_bounds( scene_center, scene_max_size );
+
+   dgd_echo(rc);
+   dgd_echo(scene_center);
+   dgd_echo(scene_max_size);
+
+   unsigned int nvertexes =
+      generate_sky_arrays( scene_max_size, n.sky_angle(), n.sky_color(),
+                           vertexes, normals, colors, texture,
+                           indexes );
+
+   GLclampf r = 0.0, g = 0.0, b = 0.0, a = 1.0;
+   
+   // Clear to last sky color
+   if (!n.sky_color().empty()) {
+       const color & last_sky_color = n.sky_color().back();
+       r = last_sky_color.r();
+       g = last_sky_color.g();
+       b = last_sky_color.b();
+   }
+   
+   glClearColor(r, g, b, a);
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   
+   glPushAttrib( GL_ENABLE_BIT );
+   glDisable(GL_DEPTH_TEST);
+   glDisable(GL_LIGHTING);
+
+   draw_arrays(vertexes, normals, colors, texture, indexes);
+
+   glEnable(GL_DEPTH_TEST);
+   glPopAttrib();
+   
+   if (glid) { 
+      glEndList(); 
+      update_list( &n, glid );
+   }
+
+   glPopMatrix();
+}
+
 }; // end of namespace vrml
 
 }; // end of namespace boxfish
